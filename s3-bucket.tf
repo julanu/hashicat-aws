61de3f87438fe8299a89c88cf90f635ef8d1c8f7
julanu <mariusjula20@gmail.com>
2022-07-20 19:22:45 +0000

    Added prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket-for-this-workshop"
-  acl    = "private"
-
+  bucket        = "my-s3-bucket-for-this-workshop"
+  acl           = "private"
+  bucket_prefix = "julanu0"
   versioning = {
     enabled = true
   }
